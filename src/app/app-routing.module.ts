6bc986087f395ca939ad8c33be93c8f99c615d9a
mich1991 <wozniak.michal91@gmail.com>
2021-06-01 15:43:55 +0100

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImagesPageComponent } from './views/images-page/images-page.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { NewsPageComponent } from './views/news-page/news-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'news' , component: NewsPageComponent},
+  {path: 'images' , component: ImagesPageComponent},
+  {path: '' , component: LandingPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
