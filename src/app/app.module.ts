e1c254f1ae38beb5d75fbe29ee948a2617aa620b
mich1991 <wozniak.michal91@gmail.com>
2021-06-03 14:34:39 +0100

    navigation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
 import { ImagesPageComponent } from './views/images-page/images-page.component';
 import { NewsPageComponent } from './views/news-page/news-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     ImagesPageComponent,
-    NewsPageComponent
+    NewsPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
